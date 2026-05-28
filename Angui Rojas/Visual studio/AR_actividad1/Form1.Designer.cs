53725f965473ef1ed6df32498f31376fc22248b7
AnguiRojasM <anguirojas04@gmail.com>
2026-05-27 20:39:51 -0500

    Actividad 1

---
+++ Angui Rojas/Visual studio/AR_actividad1/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace AR_actividad1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(92, 101);
+            label1.Name = "label1";
+            label1.Size = new Size(78, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Número 1:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(92, 160);
+            label2.Name = "label2";
+            label2.Size = new Size(78, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Número 2:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(92, 240);
+            label3.Name = "label3";
+            label3.Size = new Size(82, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Resultado :";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(188, 94);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(138, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(188, 157);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(138, 27);
+            textBox2.TabIndex = 4;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(188, 237);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(138, 27);
+            textBox3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(491, 112);
+            button1.Name = "button1";
+            button1.Size = new Size(176, 29);
+            button1.TabIndex = 6;
+            button1.Text = "Imprimir";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(493, 212);
+            button2.Name = "button2";
+            button2.Size = new Size(174, 29);
+            button2.TabIndex = 7;
+            button2.Text = "Sumar";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Angui Rojas";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ Angui Rojas/Visual studio/AR_actividad1/Form1.cs
@@ -0,0 +1,31 @@
+namespace AR_actividad1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(" Hola Mundo ");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int num1, num2, resultado;
+            num1 = int.Parse(textBox1.Text);
+            num2 = int.Parse(textBox2.Text);
+            resultado = num1 + num2;
+            textBox3.Text = resultado.ToString();
+
+
+        }
+    }
+}
