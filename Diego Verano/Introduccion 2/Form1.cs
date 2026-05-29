30979ad7c4ed5a3cf7690f9ed8942d3d6bbb1a12
Diego_v <271601461+Diego-Verano@users.noreply.github.com>
2026-05-29 15:27:20 -0500

    Add files via upload

---
+++ Diego Verano/Introduccion 2/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Introduccion_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string nombre = "Diego"; //Almacena texto || requiere comillas dobles
+            char inicial = '@'; //Almacenan caracteres únicos || requiere comillas simples
+            int tallaPolo = 16; //Almacenan números enteros o negativos || requier 4 bytes
+            long numeroLargo = 1234567890; //Almacenan números enteros o negativos muy grandes || requier 8 bytes
+            float tallaCamisa = 15.5f; //Almacenan números decimales, se debe agregar la letra "f" al final del número || requier 4 bytes
+            double tallazapato = 47.2; //Almacenan números decimales || requiere 8 bytes
+            bool online = false; //Almacena valores verdaderos o falsos || requiere 1 byte
+            var variable = "Hola"; //Almacena cualquier tipo de dato, pero no es recomendable usarlo || requiere 16 bytes
+                      
+            MessageBox.Show("Hola " + txtnombre.Text + " Bienvenido" + "\n" + "Tienes una talla de camisa de " + tallaCamisa.ToString());
+            //MessageBox.Show("Esta es mi propia introducción a C#");
+            //MessageBox.Show("Esta introducción traerá una destrucción caótica a mi propia paz");
+            //MessageBox.Show("Pero no me importa, porque el conocimiento es poder");
+
+            nombre= txtnombre.Text;
+            inicial = Convert.ToChar(txtinicial.Text);
+            tallaPolo = Convert.ToInt32(txttallapolo.Text);
+            online = Convert.ToBoolean(txtonline.Text);
+
+            MessageBox.Show(txtnombre.Text + "\n" + "Tu inicial es " + txtinicial.Text + "\n " + "Tu talla de polo es" 
+                + txttallapolo.Text + "\n " + "Y tu estado online es " +    txtonline.Text);
+
+
+        }
+    }
+}
---
+++ Diego Verano/Introduccion 2/Menú.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Introduccion_2
+{
+    partial class Menú
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(199, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Menú Introductorio";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(209, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "by Diego Verano";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(195, 229);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // Menú
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 485);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Menú";
+            this.Text = "Menú";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
