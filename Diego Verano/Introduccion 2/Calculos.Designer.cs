f0653e2e1a86429051598156d7c16f475f940d28
Diego Verano <1700140@senati.pe>
2026-05-31 12:14:41 -0500

    Cambio7

---
+++ Diego Verano/Introduccion 2/Calculos.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Introduccion_2
+{
+    partial class Calculos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnsuma = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnsuma
+            // 
+            this.btnsuma.Location = new System.Drawing.Point(41, 340);
+            this.btnsuma.Name = "btnsuma";
+            this.btnsuma.Size = new System.Drawing.Size(173, 76);
+            this.btnsuma.TabIndex = 0;
+            this.btnsuma.Text = "Suma sana";
+            this.btnsuma.UseVisualStyleBackColor = true;
+            this.btnsuma.Click += new System.EventHandler(this.btnsuma_Click);
+            // 
+            // Calculos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnsuma);
+            this.Name = "Calculos";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnsuma;
+    }
+}
---
+++ Diego Verano/Introduccion 2/Calculos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Introduccion_2
+{
+    public partial class Calculos : Form
+    {
+        public Calculos()
+        {
+            InitializeComponent();
+        }
+
+        private void btnsuma_Click(object sender, EventArgs e)
+        {
+            int num1, num2, resultado;
+
+            num1 = 20;
+            num2 = 43;
+
+            resultado = num1 + num2;
+
+            MessageBox.Show("Nuestro resultado es: " + resultado.ToString()); 
+
+            
+
+        }
+    }
+}
