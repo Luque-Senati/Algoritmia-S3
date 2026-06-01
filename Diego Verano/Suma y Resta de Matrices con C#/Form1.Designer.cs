45451139ea3f36be682a9e8338bea6d814fd3fa3
Diego Verano <1700140@senati.pe>
2026-06-01 12:28:30 -0500

    Cambio9

---
+++ Diego Verano/Suma y Resta de Matrices con C#/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Suma_y_Resta_de_Matrices_con_C_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.rbtndospor = new System.Windows.Forms.RadioButton();
+            this.rbtntrespor = new System.Windows.Forms.RadioButton();
+            this.lbloperacion = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.btnsuma = new System.Windows.Forms.Button();
+            this.btnresta = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(189, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Suma y Resta de Matrices";
+            // 
+            // rbtndospor
+            // 
+            this.rbtndospor.AutoSize = true;
+            this.rbtndospor.Location = new System.Drawing.Point(125, 116);
+            this.rbtndospor.Name = "rbtndospor";
+            this.rbtndospor.Size = new System.Drawing.Size(48, 20);
+            this.rbtndospor.TabIndex = 1;
+            this.rbtndospor.TabStop = true;
+            this.rbtndospor.Text = "2x2";
+            this.rbtndospor.UseVisualStyleBackColor = true;
+            // 
+            // rbtntrespor
+            // 
+            this.rbtntrespor.AutoSize = true;
+            this.rbtntrespor.Location = new System.Drawing.Point(368, 116);
+            this.rbtntrespor.Name = "rbtntrespor";
+            this.rbtntrespor.Size = new System.Drawing.Size(48, 20);
+            this.rbtntrespor.TabIndex = 1;
+            this.rbtntrespor.TabStop = true;
+            this.rbtntrespor.Text = "3x3";
+            this.rbtntrespor.UseVisualStyleBackColor = true;
+            // 
+            // lbloperacion
+            // 
+            this.lbloperacion.AutoSize = true;
+            this.lbloperacion.Location = new System.Drawing.Point(178, 247);
+            this.lbloperacion.Name = "lbloperacion";
+            this.lbloperacion.Size = new System.Drawing.Size(14, 16);
+            this.lbloperacion.TabIndex = 2;
+            this.lbloperacion.Text = "+";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(370, 247);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "=";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 189);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(166, 142);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(198, 189);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(166, 142);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(390, 189);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 51;
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(166, 142);
+            this.dataGridView3.TabIndex = 3;
+            // 
+            // btnsuma
+            // 
+            this.btnsuma.Location = new System.Drawing.Point(137, 366);
+            this.btnsuma.Name = "btnsuma";
+            this.btnsuma.Size = new System.Drawing.Size(133, 43);
+            this.btnsuma.TabIndex = 4;
+            this.btnsuma.Text = "Suma";
+            this.btnsuma.UseVisualStyleBackColor = true;
+            // 
+            // btnresta
+            // 
+            this.btnresta.Location = new System.Drawing.Point(283, 366);
+            this.btnresta.Name = "btnresta";
+            this.btnresta.Size = new System.Drawing.Size(133, 43);
+            this.btnresta.TabIndex = 4;
+            this.btnresta.Text = "Restar";
+            this.btnresta.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 499);
+            this.Controls.Add(this.btnresta);
+            this.Controls.Add(this.btnsuma);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbloperacion);
+            this.Controls.Add(this.rbtntrespor);
+            this.Controls.Add(this.rbtndospor);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rbtndospor;
+        private System.Windows.Forms.RadioButton rbtntrespor;
+        private System.Windows.Forms.Label lbloperacion;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Button btnsuma;
+        private System.Windows.Forms.Button btnresta;
+    }
+}
+
---
+++ Diego Verano/Suma y Resta de Matrices con C#/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("Suma y Resta de Matrices con C#")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Suma y Resta de Matrices con C#")]
+[assembly: AssemblyCopyright("Copyright © HP 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("f33cc01b-f7ed-4c36-95fb-f8f50ea280e5")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
