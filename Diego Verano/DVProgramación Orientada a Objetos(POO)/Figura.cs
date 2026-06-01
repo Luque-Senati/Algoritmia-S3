135466e954657d4e9f8bf5e5dfe7eee4c57b6177
Diego Verano <1700140@senati.pe>
2026-06-01 15:42:03 -0500

    cambio13

---
+++ Diego Verano/DVProgramación Orientada a Objetos(POO)/Figura.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVProgramación_Orientada_a_Objetos_POO_
+{
+    internal class Figura
+    {
+        public string area = "";
+        public string perimetro = "";
+
+    }
+}
