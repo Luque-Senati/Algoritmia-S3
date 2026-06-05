8921feacf6a9eea70183c0658917c16920ba56db
Diego Verano <1700140@senati.pe>
2026-06-05 15:57:28 -0500

    cambio24

--- Diego Verano/DVIf Anidados/IfAnidadoClass.cs
+++ Diego Verano/DVIf Anidados/IfAnidadoClass.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace DVIf_Anidados
 {
     internal class IfAnidadoClass
     {
-        static void Main()
+        static void Man()
         {
             try
             {
--- Diego Verano/DVIf Anidados/Program.cs
+++ Diego Verano/DVIf Anidados/Program.cs
@@ -17,6 +17,8 @@ namespace DVIf_Anidados
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            
         }
     }
 }
